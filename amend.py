399541e314339c2cfe5a5ceb854cee0cf5e5a344
Michael Jones <m.pricejones@gmail.com>
2014-06-14 00:02:38 +0100

    Rewrite to use custom Graph object
    We switch to a custom Graph object which attempts to take care of all
    the re-writing of the commit graph after any changes have taken place.
    
    It is an early version and pretty rough around the edges but it wraps
    the commit graph walking and return wrapped commits which can have their
    messages edited and the changes will be tracked and written into to the
    repository on request.
    
    Lots of ugly little bits. We always write new commits even when nothing
    has changed. We don't have a clean way to return the new head of the
    branch. Other little things like that.
    
    Makes for a nice API as far as being able to change a commit message and
    write it all to disk.

--- amend.py
+++ amend.py
@@ -1,9 +1,108 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
 import sys
 import os
 
 from pygit2 import Repository, discover_repository, GIT_SORT_TOPOLOGICAL
+
+
+def first(iterable):
+
+    for entry in iterable:
+        return entry
+
+
+class Node:
+
+    def __init__(self, commit, children):
+
+        self.commit = commit
+        self.children = children
+        self.overrides = {}
+
+    @property
+    def id(self):
+        return self.commit.id
+
+    @property
+    def message(self):
+        if 'message' in self.overrides:
+            return self.overrides['message']
+
+        return self.commit.message
+
+    @message.setter
+    def message(self, message):
+        self.overrides['message'] = message
+
+    def write(self, repo, node_lookup):
+
+        def parent_id(id_):
+            try:
+                return node_lookup[id_].id
+            except KeyError:
+                return id_
+
+        parents = list(map(parent_id, self.commit.parent_ids))
+
+        oid = repo.create_commit(
+            None,
+            self.commit.author,
+            self.commit.committer,
+            self.message,
+            self.commit.tree.id,
+            parents
+            )
+
+        # Get new commit from the repo
+        self.commit = repo.get(oid)
+
+
+class Graph:
+
+    def __init__(self, repo):
+        self.repo = repo
+        self.head_nodes = {}
+        self.children = defaultdict(list)
+        self.nodes = {}
+        self.visited = []
+
+    def walk(self, oid):
+
+        walker = self.repo.walk(oid, GIT_SORT_TOPOLOGICAL)
+
+        commit = first(walker)
+
+        node = Node(commit, [])
+        self.head_nodes[commit.id] = node
+
+        # Record this node against its parents' ids as we can only find out parents not children so
+        # we create a look up so that later we can find the children of a node in order to wire up
+        # our custom node graph
+        for entry in commit.parent_ids:
+            self.children[entry].append(commit)
+
+        self.visited.append(node)
+        self.nodes[node.id] = node
+        yield node
+
+        for entry in walker:
+
+            children = self.children[entry.id]
+            node = Node(entry, children)
+
+            self.visited.append(node)
+            self.nodes[node.id] = node
+            yield node
+
+    def write(self):
+
+        for node in reversed(self.visited):
+            node.write(self.repo, self.nodes)
+
+        return self.visited[0].id
+
 
 def main(args):
 
@@ -22,46 +121,33 @@ def main(args):
     latest_oid = current_branch.target
     latest_commit = repository.get(latest_oid)
 
-    history = []
+    graph = Graph(repository)
 
     found = False
-    for commit in repository.walk(repository.head.target, GIT_SORT_TOPOLOGICAL):
+    for commit in graph.walk(repository.head.target):
         if commit.id == old_commit.id:
             found = True
             break
-        history.insert(0, commit)
 
     if not found:
         return 1
 
-    commit = repository.create_commit(
-        current_branch.name,
-        old_commit.author,
-        old_commit.committer,
-        'my new commit message again',
-        old_commit.tree.id,
-        old_commit.parent_ids
-        )
+    # Set the commit message
+    commit.message = 'My new commit message'
 
-    prev_commit = commit
-    for entry in history:
-        prev_commit = repository.create_commit(
-            current_branch.name,
-            entry.author,
-            entry.committer,
-            entry.message,
-            entry.tree.id,
-            [prev_commit]
-            )
+    oid = graph.write()
+
+    current_branch.target = oid
 
     # Add reflog entry for the change we've just made to the branch
-    current_branch.log_append(prev_commit, old_commit.author, "reword: message for %s" % sha1)
+    current_branch.log_append(oid, old_commit.author, "reword: message for %s" % sha1)
 
     head = repository.lookup_reference("HEAD")
-    head.log_append(prev_commit, old_commit.author,
+    head.log_append(oid, old_commit.author,
                     "reword: switching to new %s" % current_branch.name)
 
     return 0
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:])) 
+
